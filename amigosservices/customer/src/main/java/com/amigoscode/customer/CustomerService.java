852b68edeb3601fec143b8d56403936842b2d1b4
dimmxx@gmail.com <dimmxx@gmail.com>
2022-03-27 06:25:27 -0700

    after notification implementation

--- amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ amigosservices/customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,28 +2,45 @@ package com.amigoscode.customer;
 
 import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
+import com.amigoscode.clients.notification.NotificationClient;
+import com.amigoscode.clients.notification.NotificationRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository, RestTemplate restTemplate, FraudClient fraudClient) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final NotificationClient notificationClient;
+    private final FraudClient fraudClient;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
             .firstName(request.firstName())
             .lastName(request.lastName())
             .email(request.email())
             .build();
-
+        // todo: check if email valid
+        // todo: check if email not taken
         customerRepository.saveAndFlush(customer);
 
-        FraudCheckResponse checkResponse = fraudClient.isFraudster(customer.getId());
+        FraudCheckResponse fraudCheckResponse =
+            fraudClient.isFraudster(customer.getId());
 
-
-
-        if(checkResponse != null && checkResponse.isFraudster()){
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
 
+        // todo: make it async. i.e add to queue
+        notificationClient.sendNotification(
+            new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome to Amigoscode...",
+                    customer.getFirstName())
+            )
+        );
 
     }
 }
